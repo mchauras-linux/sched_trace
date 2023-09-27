5e2cd3fe50437de260eccf0099bdbbf94d2d05b1
Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
2023-09-27 12:09:59 +0530

    Script added
    Signed-off-by: Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
-
+cleanup {
+	rmmod sched_trace.ko
+}
 git pull
 
 make KERNEL_SRC=$PWD/../linux-mchauras
+insmod sched_trace.ko
+
+echo 1 > /sys/kernel/debug/tracing/events/sched/sched_update_nr_running/enable
+echo 0 > /sys/kernel/debug/tracing/events/sched/sched_update_nr_running/enable
+cat /sys/kernel/debug/tracing/trace
+
+trap exit cleanup
