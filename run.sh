6d5776e01d82e3969a3dc5ec5283963159f61028
Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
2023-09-27 12:14:48 +0530

    Updated
    Signed-off-by: Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ make KERNEL_SRC=$PWD/../linux-mchauras
 insmod sched_trace.ko
 
 echo 1 > /sys/kernel/debug/tracing/events/sched/sched_update_nr_running/enable
+sleep 1
 echo 0 > /sys/kernel/debug/tracing/events/sched/sched_update_nr_running/enable
 cat /sys/kernel/debug/tracing/trace
 
